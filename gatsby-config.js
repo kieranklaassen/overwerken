2af7b9ec9bb8734c3de8bd1b08fc8e8936057081
Kieran Klaassen <kieranklaassen@gmail.com>
2020-01-21 19:56:22 +0100

    feat: init overwerken website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,53 +1,48 @@
-require(`dotenv`).config()
+require(`dotenv`).config();
 
 module.exports = {
   siteMetadata: {
-    title: `Phoenix.`,
-    description: `A personal blogging theme for Gatsby with great typography and dark mode.`,
+    title: `Overwerken`,
+    description: `Podcast: Overwerken ontmoet iedere aflevering een van de 9 miljoen werkende Nederlanders. Waarom diep doen als het ook oppervlakkig kan? Een geraffineerd evenwicht tussen non-journalistiek en non-sense.`,
     siteUrl: process.env.SITE_URL,
-    startUrl: `/`,
-    copyright: `© YYYY Phoenix. All rights reserved.`,
+    startUrl: `/podcast`,
+    copyright: `© YYYY Overwerken. All rights reserved.`,
     icon: `src/images/icon.png`,
-    color: `#3C64F1`,
+    color: `#AC1563`,
     menuLinks: [
-      {
-        name: `Home`,
-        link: `/`,
-      },
-      {
-        name: `Blog`,
-        link: `/blog`,
-      },
+      // {
+      //   name: `Home`,
+      //   link: `/`
+      // },
+      // {
+      //   name: `Blog`,
+      //   link: `/blog`
+      // },
       {
         name: `Podcast`,
-        link: `/podcast`,
-      },
-      {
-        name: `Porfolio`,
-        link: `/portfolio`,
-      },
-      {
-        name: `Photography`,
-        link: `/photography`,
-      },
+        link: `/podcast`
+      }
+      // {
+      //   name: `Porfolio`,
+      //   link: `/portfolio`
+      // },
+      // {
+      //   name: `Photography`,
+      //   link: `/photography`
+      // }
     ],
     socialLinks: [
       {
-        name: `Twitter`,
-        url: `https://twitter.com`,
-        icon: `twitter`,
+        name: `Instagram`,
+        url: `https://www.instagram.com/overwerken/`,
+        icon: `instagram`
       },
       {
-        name: `Github`,
-        url: `https://github.com`,
-        icon: `github`,
-      },
-      {
-        name: `Youtube`,
-        url: `https://youtube.com`,
-        icon: `youtube`,
-      },
-    ],
+        name: `Soundcloud`,
+        url: `https://soundcloud.com/overwerken`,
+        icon: `youtube`
+      }
+    ]
   },
   plugins: [
     // This is a list of all themes that this starter is using.
@@ -59,24 +54,28 @@ module.exports = {
     {
       resolve: `@arshad/gatsby-theme-podcast-core`,
       options: {
-        feedUrl: `https://feeds.megaphone.fm/travelgenius`,
+        feedUrl: `http://feeds.soundcloud.com/users/soundcloud:users:589671126/sounds.rss`,
         podcast: {
-          name: `Travel Genuis`,
-          description: `Eligendi nisi nobis nisi voluptate. Corporis deserunt provident hic numquam. Veritatis vero necessitatibus adipisci cumque voluptate rerum at.`,
-          image: `assets/images/podcast.jpg`,
+          name: `Overwerken Podcast`,
+          description: `Overwerken ontmoet iedere aflevering een van de 9 miljoen werkende Nederlanders. Waarom diep doen als het ook oppervlakkig kan? Een geraffineerd evenwicht tussen non-journalistiek en non-sense.`,
+          image: `assets/images/podcast.png`,
           social: [
             {
               name: `Apple Podcast`,
-              url: `https://itunes.apple.com`,
+              url: `https://podcasts.apple.com/us/podcast/id1454167016`
+            },
+            {
+              name: `Spotify Podcast`,
+              url: "https://open.spotify.com/show/4NNG8myXw15eq8LHDGaeQp"
             },
             {
               name: `Google Podcast`,
-              url: `https://podcasts.google.com`,
-            },
-          ],
-        },
-      },
+              url: `https://www.google.com/podcasts?feed=aHR0cDovL2ZlZWRzLnNvdW5kY2xvdWQuY29tL3VzZXJzL3NvdW5kY2xvdWQ6dXNlcnM6NTg5NjcxMTI2L3NvdW5kcy5yc3M%3D`
+            }
+          ]
+        }
+      }
     },
-    `@arshad/gatsby-theme-phoenix`,
-  ],
-}
+    `@arshad/gatsby-theme-phoenix`
+  ]
+};
